a646e18e33095cd75370369c10177665104bb184
Kelvin DeCosta <decostakelvin@gmail.com>
2020-05-29 13:01:50 +0300

    Purge files prior to refactoring

--- lib/config/session.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-from datetime import datetime
-
-# Default device
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Timestamp
-TIMESTAMP = datetime.now().strftime("%Y-%m-%d-%H-%M")
